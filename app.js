7e3a218a0272742347c3a8ce3f2c2372e7c83667
Matthias <matthias.debus@gmail.com>
2014-11-24 16:40:38 +0100

    v0.5

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ var app = angular.module('deliciousDashboardApp', ['ngRoute','ngStorage'])
 	.otherwise({ redirectTo: "/" });
 });
 
-var username = "bl00p";
-var mytags = ['news','socialnetworking','bitcoin','nxt','mwork','tools','1-click','web2.0','musik','clubs','latest'];
+//var username = "bl00p";
+//var mytags = ['news','socialnetworking','bitcoin','nxt','mwork','tools','1-click','web2.0','musik','clubs','latest'];
+//var privateKey = "-FynrgFmtek897_Z93-mME6OwS41XnWHOpKs8zXY8sQ=";
 var titleLimit = 34;
-var privateKey = "-FynrgFmtek897_Z93-mME6OwS41XnWHOpKs8zXY8sQ=";
 var deliciousURL = "http://feeds.delicious.com/v2/json/"
 
-app.controller('mainCtrl', function($scope, $localStorage) {
+app.controller('mainCtrl', function($scope, $localStorage, $location) {
 	$scope.$storage = $localStorage;
-	$scope.mytags = mytags;
+	$scope.mytags = $scope.$storage.favoritetags.split(',');
+	if (!$scope.$storage.username) { $location.path('/settings'); }  
+	//$scope.mytags = mytags;
 	$scope.titleLimit = titleLimit;
 	angular.forEach(document.querySelectorAll('.main-search'), function(elem) { elem.focus(); });
 	$scope.toggleTags = function(){
@@ -30,7 +32,9 @@ app.controller('mainCtrl', function($scope, $localStorage) {
 app.controller('getLinksCtrl', function($scope,$http) {
 	if (!$scope.tag) throw new Error("No Tag for LinksController");
 	if ($scope.tag == 'latest') { $scope.tag = '' } else { $scope.tag = '/' + $scope.tag };
-	$http.get(deliciousURL + username + $scope.tag + '?count=100;private=' + privateKey).then(function(articlesResponse) {
+	var username = $scope.$storage.username;
+	if ($scope.$storage.privatekey) { var privateKey = ';private=' + $scope.$storage.privatekey; }
+	$http.get(deliciousURL + username + $scope.tag + '?count=100' + privateKey).then(function(articlesResponse) {
     		$scope.links = articlesResponse.data;
 	})
 });
