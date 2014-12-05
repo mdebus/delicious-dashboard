f0cb90b21e21b7f4c312169fac39b425e66861a4
Matthias <matthias.debus@gmail.com>
2014-12-05 08:57:03 +0100

    v0.6

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+//var privateKey = "-FynrgFmtek897_Z93-mME6OwS41XnWHOpKs8zXY8sQ=";
+var titleLimit = 34;
+var deliciousURL = "http://feeds.delicious.com/v2/json/"
+
 var app = angular.module('deliciousDashboardApp', ['ngRoute','ngStorage'])
 .config(function ($routeProvider) {
   $routeProvider
@@ -8,38 +12,19 @@ var app = angular.module('deliciousDashboardApp', ['ngRoute','ngStorage'])
 	.otherwise({ redirectTo: "/" });
 });
 
-//var username = "bl00p";
-//var mytags = ['news','socialnetworking','bitcoin','nxt','mwork','tools','1-click','web2.0','musik','clubs','latest'];
-//var privateKey = "-FynrgFmtek897_Z93-mME6OwS41XnWHOpKs8zXY8sQ=";
-var titleLimit = 34;
-var deliciousURL = "http://feeds.delicious.com/v2/json/"
-
 app.controller('mainCtrl', function($scope, $localStorage, $location) {
 	$scope.$storage = $localStorage;
 	$scope.mytags = $scope.$storage.favoritetags.split(',');
 	if (!$scope.$storage.username) { $location.path('/settings'); }  
-	//$scope.mytags = mytags;
 	$scope.titleLimit = titleLimit;
 	angular.forEach(document.querySelectorAll('.main-search'), function(elem) { elem.focus(); });
-	$scope.toggleTags = function(){
-		if($scope.mytags == 'latest') {
-			$scope.mytags = mytags;
-		} else {
-			$scope.mytags = ['latest'];
-		}
-	}
 });
+
 app.controller('getLinksCtrl', function($scope,$http) {
 	if (!$scope.tag) throw new Error("No Tag for LinksController");
 	if ($scope.tag == 'latest') { $scope.tag = '' } else { $scope.tag = '/' + $scope.tag };
-	var username = $scope.$storage.username;
 	if ($scope.$storage.privatekey) { var privateKey = ';private=' + $scope.$storage.privatekey; }
-	$http.get(deliciousURL + username + $scope.tag + '?count=100' + privateKey).then(function(articlesResponse) {
+	$http.get(deliciousURL + $scope.$storage.username + $scope.tag + '?count=100' + privateKey).then(function(articlesResponse) {
     		$scope.links = articlesResponse.data;
 	})
 });
-
-app.controller('settingsStorageCtrl', function($scope, $localStorage) {
-//	if (!$scope.lsvalue) throw new Error("No lsvalue for settingsStorageController");
-});
-
